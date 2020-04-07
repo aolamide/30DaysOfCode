3598d20e573e91cefa5ccf5e519a07cddd7f54b8
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2020-04-07 21:29:15 +0100

    day 14 done

---
+++ dromeServer(day14).js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bp = require('body-parser');
+
+//initialize app
+const app = express();
+
+//body-parser middleware
+app.use(bp.json());
+
+let dromes = [];
+
+//isPalindrome function
+const isPalindrome = str => str === str.toLowerCase().split('').reverse().join('');
+
+//POST data
+app.post('/postWords', (req, res) => {
+  let words = req.body.words; //get the words from the req body
+  //filter the words array to only return words that are palindromes using the palindrome checker
+  dromes = words.filter(word => isPalindrome(word));
+});
+
+//GET request to get data
+app.get('/getDromes', (req, res) => {
+  return res.json({dromes});
+});
+
+const PORT = process.env.PORT || 5050;
+//listen
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
