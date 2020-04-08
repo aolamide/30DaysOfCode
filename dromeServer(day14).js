fa457e07502144fe4678ad10d70c31ebb95b55c2
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2020-04-08 11:10:59 +0100

    Add response for POST request

--- dromeServer(day14).js
+++ dromeServer(day14).js
@@ -17,6 +17,10 @@ app.post('/postWords', (req, res) => {
   let words = req.body.words; //get the words from the req body
   //filter the words array to only return words that are palindromes using the palindrome checker
   dromes = words.filter(word => isPalindrome(word));
+  return res.json({
+    status : 'success',
+    message : 'Words have been sent. Now go to /getDromes to get the palindromes'
+  });
 });
 
 //GET request to get data
